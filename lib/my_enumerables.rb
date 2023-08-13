b074b75d327cbcbeb5994da60263aa392c926085
upkl <2737384+upkl@users.noreply.github.com>
2023-08-13 17:13:13 +0200

    Solved exercises in Ruby Course → Advanced Ruby → Project: Custom Enumerables

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,62 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index(&block)
+    for index in (0...self.length)
+      block.call(self[index], index)
+    end
+    self
+  end
+
+  def my_select(&block)
+    result = []
+    for x in self
+      result << x if block.call(x)
+    end
+    result
+  end
+
+  def my_all?(&block)
+    for x in self
+      return false unless block.call(x)
+    end
+    true
+  end
+
+  def my_any?(&block)
+    for x in self
+      return true if block.call(x)
+    end
+    false
+  end
+
+  def my_none?(&block)
+    not my_any?(&block)
+  end
+
+  def my_count(&block)
+    if block.nil?
+      return self.length
+    end
+    result = 0
+    for x in self
+      result += 1 if block.call(x)
+    end
+    result
+  end
+
+  def my_map(&block)
+    result = []
+    for x in self
+      result << block.call(x)
+    end
+    result
+  end
+
+  def my_inject(acc=0, &block)
+    for x in self
+      acc = block.call(acc, x)
+    end
+    acc
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +64,11 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each(&block)
+    for x in self
+      block.call(x)
+    end
+    self
+  end
 end
+
